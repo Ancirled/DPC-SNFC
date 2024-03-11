475bc97c7ca0de37e1f57aae1e4edb4e0ef16f43
Ancirled <118651104+Ancirled@users.noreply.github.com>
2024-03-11 17:54:02 +0800

    Add files via upload

---
+++ constructGraph.m
@@ -0,0 +1,19 @@
+function [G] = construct_rnn(rho,father,rnn,gama)
+    n = length(rho);
+    a = 1;
+    visited = zeros(1,n);
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    G = graph(s,t);
+end
+
