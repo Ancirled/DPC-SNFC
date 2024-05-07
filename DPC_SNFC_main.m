acf142540502f4b57569ec35cdc46a3ed8565ab3
Ancirled <118651104+Ancirled@users.noreply.github.com>
2024-05-07 16:29:17 +0800

    Add files via upload

---
+++ DPC_SNFC_main.m
@@ -0,0 +1,32 @@
+clear all; close all; clc;
+
+%% Load dataset
+fileName = 'flame.txt';
+X = load(fileName);
+
+% fileName = 'All Datasets/real/Iris.mat';
+% load(fileName);
+% X = Iris;                                       
+% X(:,end+1) = label;                           
+
+A = X;
+A(:,end) = [];                                       
+T = X(:,end);                 % True label
+
+%% Parameters
+lemda = 10;
+gama = 3;
+
+%% Prepare
+% A=normalize(A);
+n = size(A,1);                             
+dist = CaculateDist(A);                     
+[knn,rnn] = neighbors(X,lemda);         
+rho = calculateRho(dist,knn);            
+[father] = FindSuperiorNode(rnn,dist,rho);     
+
+%% Construct graph
+G = FuzzyCorrelation(rho,father,rnn,gama);
+[labels,c] = conncomp(G);       % Clustering result
+Plot(X,labels);
+[AMI,ARI,FMI,NMI,Purity,RI] = evaluation(T,labels',n,2);  
