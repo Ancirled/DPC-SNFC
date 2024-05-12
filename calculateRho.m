26679ff2340e3a07b21e277fe828003defb37781
Ancirled <118651104+Ancirled@users.noreply.github.com>
2024-05-12 21:46:01 +0800

    Delete calculateRho.m

--- calculateRho.m
+++
@@ -1,7 +0,0 @@
-function [rho] = calculateRho(dist,knn)
-    n=size(dist,1);
-    rho = ones(n,1);
-    for i=1:n
-        rho(i) =exp(-sum(dist(i,knn{i}(1:length(knn{i}))))/length(knn{i}));
-    end
-end
